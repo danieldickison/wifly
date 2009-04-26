1abbfecaebba4ea4e27eced1a2fb9495f0dfc5d1
Daniel Dickison <danieldickison@gmail.com>
2009-04-26 00:45:47 -0500

    Simpler and I think better tilt calculation from accellerometer

--- xplane-plugin/iX-Yoke/Classes/iX_YokeAppDelegate.m
+++ xplane-plugin/iX-Yoke/Classes/iX_YokeAppDelegate.m
@@ -51,14 +51,10 @@
     yAvg = acceleration.y * kFilteringFactor + yAvg * (1.0 - kFilteringFactor);
     zAvg = acceleration.z * kFilteringFactor + zAvg * (1.0 - kFilteringFactor);
     
-    // Convert rectangular to spherical coordinates.
-    double rho = sqrt(xAvg*xAvg + yAvg*yAvg + zAvg*zAvg);
-    double theta = atan2(yAvg, xAvg);
-    double phi = acos(zAvg / rho);
-    
-    // I don't think this is quite right regarding "upside-down" angles, but it works within the (-90, +90) degree ranges.
-    pitch = phi - M_PI_2;
-    roll = theta + M_PI_2;
+    // This method gives a range of +/- 90 degrees on each axis, but only if you're rotating about one axis at a time.  If you try to tilt on both axes, then it maxes out at +/- 45 degrees.  It would probably be nicer if the range is kept constant regardless of whether you're tilting in one or two axes.
+    double ySqr = yAvg*yAvg;
+    pitch = atan2(zAvg, sqrt(ySqr + xAvg*xAvg));
+    roll = atan2(xAvg, sqrt(ySqr + zAvg*zAvg));
     
     [mainViewController updatePitch:pitch roll:roll];
 }
