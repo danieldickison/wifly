33ac13dc8c9adc1cb2997015038b78c32c9eedae
Daniel Dickison <danieldickison@gmail.com>
2009-05-10 16:07:12 -0500

    More reorganizing and implemented plugin UI for all axis control selection

---
+++ ios-app/iX-Yoke-plugin/Window.c
@@ -0,0 +1,205 @@
+/*
+ *  Window.c
+ *  iX-Yoke-plugin
+ *
+ *  Created by Daniel Dickison on 5/10/09.
+ *  Copyright 2009 Daniel_Dickison. All rights reserved.
+ *
+ */
+
+#include "iX-Yoke-plugin.h"
+
+
+int axis_popup_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inPopupID, long inItemNumber);
+int window_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inParam1, long inParam2);
+int textfield_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inParam1, long inParam2);
+
+XPWidgetID window_id = 0;
+
+
+void destroy_window()
+{
+    if (window_id != 0)
+    {
+        XPDestroyWidget(window_id, 1);
+        window_id = 0;
+    }
+}
+
+
+void show_window()
+{
+    if (window_id == 0)
+    {
+        // Create config window.
+        debug("Creating config window...");
+        
+        int x1=200, y1=470, x2=500, y2=200;
+        window_id = XPCreateWidget(x1, y1, x2, y2, 0, "iX-Yoke", 1, NULL, xpWidgetClass_MainWindow);
+        XPSetWidgetProperty(window_id, xpProperty_MainWindowHasCloseBoxes, 1);
+        XPAddWidgetCallback(window_id, window_callback);
+        
+        x1 += 10; y1 -= 30; x2 -= 10; y2 += 10;
+        XPWidgetID subwin = XPCreateWidget(x1, y1, x2, y2, 1, "", 0, window_id, xpWidgetClass_SubWindow);
+        XPSetWidgetProperty(subwin, xpProperty_SubWindowType, xpSubWindowStyle_SubWindow);
+        XPAddWidgetCallback(subwin, XPUFixedLayout);
+        
+        x1 += 10; x2 -= 10; y2 = y1;
+        for (int i = 0; i < kNumAxes; i++)
+        {
+            y1 = y2-10; y2 = y1-20;
+            iXControlAxisRef control = get_axis(i);
+            
+            // Axis title.
+            XPCreateWidget(x1, y1, x1+100, y2, 1, control->title, 0, subwin, xpWidgetClass_Caption);
+            
+            // Popup
+            control->popup_widget = XPCreatePopup(x1+120, y1, x2, y2, 1, axis_choices, subwin);
+            XPAddWidgetCallback(control->popup_widget, axis_popup_callback);
+            XPSetWidgetProperty(control->popup_widget, xpProperty_PopupCurrentItem, control->type);
+            
+            // Min, Max, progress
+            y1 = y2-5; y2 = y1-20;
+            control->min_widget = XPCreateWidget(x1, y1, x1+50, y2, 1, "-1", 0, subwin, xpWidgetClass_TextField);
+            control->max_widget = XPCreateWidget(x2-50, y1, x2, y2, 1, "1", 0, subwin, xpWidgetClass_TextField);
+            XPAddWidgetCallback(control->min_widget, textfield_callback);
+            XPAddWidgetCallback(control->max_widget, textfield_callback);
+            
+            control->progress_widget = XPCreateWidget(x1+60, y1, x2-60, y2, 1, "", 0, subwin, xpWidgetClass_Progress);
+            XPSetWidgetProperty(control->progress_widget, xpProperty_ProgressMin, 0);
+            XPSetWidgetProperty(control->progress_widget, xpProperty_ProgressMax, 1024);
+        }
+    }
+    
+    // Set all the popups and text fields' values.
+    for (int i = 0; i < kNumAxes; i++)
+    {
+        iXControlAxisRef control = get_axis(i);
+        char str[7];
+        snprintf(str, 7, "%f", (double)control->min);
+        XPSetWidgetDescriptor(control->min_widget, str);
+        snprintf(str, 7, "%f", (double)control->max);
+        XPSetWidgetDescriptor(control->max_widget, str);
+    }
+    
+    XPShowWidget(window_id);
+}
+
+
+
+int update_window()
+{
+    if (!window_id) return 0;
+    
+    for (int i = 0; i < kNumAxes; i++)
+    {
+        iXControlAxisRef control = get_axis(i);
+        XPWidgetID progress = control->progress_widget;
+        float value = control->value;
+        long intValue = (long)(1024.0f * value);
+        XPSetWidgetProperty(progress, xpProperty_ProgressPosition, intValue);
+    }
+    
+    return 1;
+}
+
+
+
+int axis_popup_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inPopupID, long inItemNumber)
+{
+    if (inMessage != xpMessage_PopupNewItemPicked)
+    {
+        return 0;
+    }
+    
+    iXControlAxisRef control = NULL;
+    for (int i = 0; i < kNumAxes; i++)
+    {
+        if (get_axis(i)->popup_widget == (void*)inPopupID)
+        {
+            control = get_axis(i);
+            break;
+        }
+    }
+    if (!control) return 0;
+    control->type = inItemNumber;
+    return 1;
+}
+
+
+int window_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inParam1, long inParam2)
+{
+	if (inMessage == xpMessage_CloseButtonPushed)
+	{
+        destroy_window();
+        return 1;
+    }
+    
+    return 0;
+}
+
+
+
+int textfield_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inParam1, long inParam2)
+{
+    if (inMessage == xpMsg_KeyPress)
+    {
+        // Only allow numeric, decimal point and minus characters.
+        XPKeyState_t *key_state = (void *)inParam1;
+        char key = key_state->key;
+        char vkey = key_state->vkey;
+        if (key == '\n' || key == '\r')
+        {
+            XPLoseKeyboardFocus(inWidget);
+            return 1;
+        }
+        if ((key_state->flags & (xplm_ControlFlag | xplm_ShiftFlag | xplm_OptionAltFlag)) ||
+            ((key < '0' || key > '9') && (key != '.') && (key != '-') &&
+             (key != 0x7f) && (key != '\b') &&
+             (vkey != XPLM_VK_LEFT) && (vkey != XPLM_VK_RIGHT) &&
+             (vkey != XPLM_VK_ESCAPE)))
+        {
+            return 1;
+        }
+    }
+    else if (inMessage == xpMsg_KeyLoseFocus)
+    {
+        XPWidgetID textfield = inWidget;
+        iXControlAxisRef control = NULL;
+        int isMin = 0;
+        for (int i = 0; i < kNumAxes; i++)
+        {
+            iXControlAxisRef this_control = get_axis(i);
+            if (this_control->min_widget == textfield)
+            {
+                isMin = 1;
+                control = this_control;
+                break;
+            }
+            else if (this_control->max_widget == textfield)
+            {
+                isMin = 0;
+                control = this_control;
+                break;
+            }
+        }
+        if (!control) return 0;
+        
+        char str[32];
+        XPGetWidgetDescriptor(textfield, str, 32);
+        str[31] = '\0'; // Null-terminate in case user typed >31 chars.
+        float value = atof(str);
+        if (isMin)
+        {
+            control->min = value;
+        }
+        else
+        {
+            control->max = value;
+        }
+        
+        return 1;
+    }
+    return 0;
+}
+
