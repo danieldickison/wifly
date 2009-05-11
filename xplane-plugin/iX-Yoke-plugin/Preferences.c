676463682c85485a3f82188f9cba7b949981ab15
Daniel Dickison <danieldickison@gmail.com>
2009-05-11 00:41:21 -0500

    Added plugin settings presets and UI to select them Not yet implemented is saving custom presets and loading them from prefs file

---
+++ xplane-plugin/iX-Yoke-plugin/Preferences.c
@@ -0,0 +1,173 @@
+/*
+ *  Preferences.c
+ *  iX-Yoke-plugin
+ *
+ *  Created by Daniel Dickison on 5/11/09.
+ *  Copyright 2009 Daniel_Dickison. All rights reserved.
+ *
+ */
+
+#include "iX-Yoke-plugin.h"
+
+typedef struct {
+    iXControlType type;
+    float min;
+    float max;
+} iXPresetAxis;
+
+typedef struct {
+    char name[64];
+    iXPresetAxis axes[4];
+} iXPreset;
+
+
+static int current_pre = -1;
+static int num_presets = 0;
+static iXPreset presets[32];
+
+static const int num_readonly_presets = 3;
+static const iXPreset readonly_presets[3] = {
+    {
+        "Stick and Throttle (read-only)",
+        {
+            {kAxisControlRoll, -1.0, 1.0},
+            {kAxisControlPitch, -1.0, 1.0},
+            {kAxisControlYaw, -1.0, 1.0},
+            {kAxisControlThrottle, 0.0, 1.0}
+        }
+    },
+    {
+        "Auto-coordinate Rudder (read-only)",
+        {
+            {kAxisControlRollAndYaw, -1.0, 1.0},
+            {kAxisControlPitch, -1.0, 1.0},
+            {kAxisControlOff, -1.0, 1.0},
+            {kAxisControlThrottle, 0.0, 1.0}
+        }
+    },
+    {
+        "Helicopter Stick and Collective (read-only)",
+        {
+            {kAxisControlRoll, -1.0, 1.0},
+            {kAxisControlPitch, -1.0, 1.0},
+            {kAxisControlYaw, -1.0, 1.0},
+            {kAxisControlPropPitch, 10.0, -1.0}
+        }
+    }
+};
+
+
+FILE *get_prefs_file(char *mode)
+{
+    char path[512];
+    XPLMGetSystemPath(path);
+    strcat(path, "Resources");
+    strcat(path, XPLMGetDirectorySeparator());
+    strcat(path, "preferences");
+    strcat(path, XPLMGetDirectorySeparator());
+    strcat(path, "iX-Yoke.prf");
+#if APL
+    MacToUnixPath(path, path, 512);
+#endif
+    
+    return fopen(path, mode);
+}
+
+
+void load_prefs()
+{
+    FILE *f = get_prefs_file("r");
+    //...
+    fclose(f);
+}
+
+
+void save_prefs()
+{
+    FILE *f = get_prefs_file("w");
+    //...
+    fclose(f);
+}
+
+
+int get_preset_names(char **outNames)
+{
+    int i = 0;
+    for (int j = 0; j < num_readonly_presets; j++)
+    {
+        outNames[i++] = (char*)readonly_presets[j].name;
+    }
+    for (int j = 0; j < num_presets; j++)
+    {
+        outNames[i++] = (char*)presets[j].name;
+    }
+    return num_presets + num_readonly_presets;
+}
+
+
+int current_preset()
+{
+    return current_pre;
+}
+
+
+void set_current_preset(int i)
+{
+    current_pre = i;
+    if (i >= 0 && i < (num_readonly_presets + num_presets))
+    {
+        iXPreset preset;
+        if (i < num_readonly_presets)
+            preset = readonly_presets[i];
+        else
+            preset = presets[i - num_readonly_presets];
+        
+        for (int j = 0; j < kNumAxes; j++)
+        {
+            iXControlAxisRef control = get_axis(j);
+            iXPresetAxis pre_axis = preset.axes[j];
+            control->type = pre_axis.type;
+            control->min = pre_axis.min;
+            control->max = pre_axis.max;
+        }
+    }
+}
+
+
+void save_preset_as(const char *inName)
+{
+    iXPreset new_preset;
+    strncpy(new_preset.name, inName, 64);
+    new_preset.name[63] = '\0';
+    for (int i = 0; i < kNumAxes; i++)
+    {
+        iXControlAxisRef control = get_axis(i);
+        new_preset.axes[i].type = control->type;
+        new_preset.axes[i].min = control->min;
+        new_preset.axes[i].max = control->max;
+    }
+    
+    // On overflow, just replace the last preset.  Hacky, but I think good enough for now.
+    if (num_presets == 32) num_presets--;
+    
+    presets[num_presets] = new_preset;
+    current_pre = num_readonly_presets + num_presets;
+    num_presets++;
+}
+
+
+void delete_preset(int i)
+{
+    if (i < num_readonly_presets) return;
+    
+    num_presets--;
+    for (int j = (i - num_readonly_presets); j < num_presets; j++)
+    {
+        presets[j] = presets[j+1];
+    }
+    
+    if (i == current_pre)
+    {
+        current_pre = -1;
+    }
+}
--- xplane-plugin/iX-Yoke-plugin/Window.c
+++ xplane-plugin/iX-Yoke-plugin/Window.c
@@ -10,11 +10,16 @@
 #include "iX-Yoke-plugin.h"
 
 
+int preset_popup_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inPopupID, long inItemNumber);
 int axis_popup_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inPopupID, long inItemNumber);
 int window_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inParam1, long inParam2);
 int textfield_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inParam1, long inParam2);
 
 XPWidgetID window_id = 0;
+XPWidgetID preset_popup_id = 0;
+
+void get_preset_menu_str(char *outStr);
+void update_settings_display();
 
 
 void destroy_window()
@@ -34,12 +39,21 @@ void show_window()
         // Create config window.
         debug("Creating config window...");
         
-        int x1=200, y1=470, x2=500, y2=200;
+        int x1=200, y1=500, x2=500, y2=200;
         window_id = XPCreateWidget(x1, y1, x2, y2, 0, "iX-Yoke", 1, NULL, xpWidgetClass_MainWindow);
         XPSetWidgetProperty(window_id, xpProperty_MainWindowHasCloseBoxes, 1);
         XPAddWidgetCallback(window_id, window_callback);
         
-        x1 += 10; y1 -= 30; x2 -= 10; y2 += 10;
+        
+        // Add preset controls.
+        x1 += 10; x2 -= 10; y1 -= 30; y2 += 10;
+        XPCreateWidget(x1, y1, x1+50, y1-20, 1, "Preset:", 0, window_id, xpWidgetClass_Caption);
+        preset_popup_id = XPCreatePopup(x1+55, y1, x2-100, y1-25, 1, "Presets", window_id);
+        XPAddWidgetCallback(preset_popup_id, preset_popup_callback);
+        
+        
+        // Add subwindow and controls for each axis.
+        y1 -= 30;
         XPWidgetID subwin = XPCreateWidget(x1, y1, x2, y2, 1, "", 0, window_id, xpWidgetClass_SubWindow);
         XPSetWidgetProperty(subwin, xpProperty_SubWindowType, xpSubWindowStyle_SubWindow);
         XPAddWidgetCallback(subwin, XPUFixedLayout);
@@ -56,7 +70,6 @@ void show_window()
             // Popup
             control->popup_widget = XPCreatePopup(x1+120, y1, x2, y2, 1, axis_choices, subwin);
             XPAddWidgetCallback(control->popup_widget, axis_popup_callback);
-            XPSetWidgetProperty(control->popup_widget, xpProperty_PopupCurrentItem, control->type);
             
             // Min, Max, progress
             y1 = y2-5; y2 = y1-20;
@@ -71,16 +84,7 @@ void show_window()
         }
     }
     
-    // Set all the popups and text fields' values.
-    for (int i = 0; i < kNumAxes; i++)
-    {
-        iXControlAxisRef control = get_axis(i);
-        char str[7];
-        snprintf(str, 7, "%f", (double)control->min);
-        XPSetWidgetDescriptor(control->min_widget, str);
-        snprintf(str, 7, "%f", (double)control->max);
-        XPSetWidgetDescriptor(control->max_widget, str);
-    }
+    update_settings_display();
     
     XPShowWidget(window_id);
 }
@@ -122,7 +126,14 @@ int axis_popup_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inP
         }
     }
     if (!control) return 0;
-    control->type = inItemNumber;
+    
+    if (control->type != inItemNumber)
+    {
+        control->type = inItemNumber;
+        set_current_preset(-1);
+        update_settings_display();
+    }
+    
     return 1;
 }
 
@@ -189,17 +200,76 @@ int textfield_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inPa
         XPGetWidgetDescriptor(textfield, str, 32);
         str[31] = '\0'; // Null-terminate in case user typed >31 chars.
         float value = atof(str);
+        int value_changed;
         if (isMin)
         {
+            value_changed = control->min != value;
             control->min = value;
         }
         else
         {
+            value_changed = control->max != value;
             control->max = value;
+        }
+        
+        if (value_changed)
+        {
+            set_current_preset(-1);
+            update_settings_display();
         }
         
         return 1;
     }
     return 0;
+}
+
+
+int preset_popup_callback(XPWidgetMessage inMessage, XPWidgetID inWidget, long inPopupID, long inItemNumber)
+{
+    if (inMessage == xpMessage_PopupNewItemPicked)
+    {
+        set_current_preset(inItemNumber - 1);
+        update_settings_display();
+        return 1;
+    }
+    return 0;
+}
+
+
+
+void update_settings_display()
+{
+    char preset_menu_str[65*48];
+    get_preset_menu_str(preset_menu_str);
+    XPSetWidgetDescriptor(preset_popup_id, preset_menu_str);
+    XPSetWidgetProperty(preset_popup_id, xpProperty_PopupCurrentItem, 1+current_preset());
+    
+    
+    // Set all the popups and text fields' values.
+    for (int i = 0; i < kNumAxes; i++)
+    {
+        iXControlAxisRef control = get_axis(i);
+        
+        XPSetWidgetProperty(control->popup_widget, xpProperty_PopupCurrentItem, control->type);
+        
+        char str[7];
+        snprintf(str, 7, "%f", (double)control->min);
+        XPSetWidgetDescriptor(control->min_widget, str);
+        snprintf(str, 7, "%f", (double)control->max);
+        XPSetWidgetDescriptor(control->max_widget, str);
+    }
+}
+
+
+void get_preset_menu_str(char *outStr)
+{
+    char *names[48];
+    int n = get_preset_names(names);
+    strcpy(outStr, "Custom");
+    for (int i = 0; i < n; i++)
+    {
+        strcat(outStr, ";");
+        strcat(outStr, names[i]);
+    }
 }
 
