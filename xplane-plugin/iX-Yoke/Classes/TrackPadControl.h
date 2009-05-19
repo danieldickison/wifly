34cad801a3d52a5e6e34c7bea2b53d63c55dfc02
Daniel Dickison <danieldickison@gmail.com>
2009-05-19 06:45:06 -0500

    Only allow tracking bounds adjustments while holding

--- xplane-plugin/iX-Yoke/Classes/TrackPadControl.h
+++ xplane-plugin/iX-Yoke/Classes/TrackPadControl.h
@@ -9,6 +9,14 @@
 #import <UIKit/UIKit.h>
 
 
+typedef enum {
+    TrackPadTouchesIgnored = 0,
+    TrackPadTouchesValueRelative,
+    TrackPadTouchesValueAbsolute,
+    TrackPadTouchesBounds
+} TrackPadInteractionMode;
+
+
 @interface TrackPadControl : UIControl
 {
     CGPoint minValue;
@@ -17,25 +25,19 @@
     CGPoint holdPoint;
     BOOL holding;
     CGRect trackingBounds;
-    BOOL absoluteTracking;
-    BOOL singleTouchCenters;
+    TrackPadInteractionMode interactionMode;
     BOOL pinchTrackingX;
     BOOL pinchTrackingY;
 }
 
 @property (nonatomic, assign) CGFloat xValue;
-@property (nonatomic, assign) CGFloat yValue;;
+@property (nonatomic, assign) CGFloat yValue;
 @property (nonatomic, assign) CGRect trackingBounds;
-
-// If absoluteTracking is true, the value point is placed exactly where single-finger touches occur rather than depending on the relative movement of drags.
-@property (nonatomic, assign) BOOL absoluteTracking;
-
-// If this is true, single touch and drag moves the center of the tracking bounds instead of setting the value point.  In this case, it's expected the value is set programatically by xValue and yValue (e.g. from accellerometer inputs).
-@property (nonatomic, assign) BOOL singleTouchCenters;
+@property (nonatomic, assign) TrackPadInteractionMode interactionMode;
 
 - (void)setMinX:(CGFloat)minx maxX:(CGFloat)maxx minY:(CGFloat)miny maxY:(CGFloat)maxY;
 
-// While "held", the raw-value crosshairs will keep updating, but the xValue and yValue properties will be pinned to the current values when it started holding.
+// While "held", touches will adjust the tracking bounds instead of the value.  The raw-value crosshairs will keep updating, but the xValue and yValue properties will be pinned to the current values when it started holding.
 @property (nonatomic, assign, getter=isHolding) BOOL holding;
 
 @end
