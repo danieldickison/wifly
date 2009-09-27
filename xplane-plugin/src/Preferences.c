10f573cdac5f279e8211e9888c52eb9a5d5992f2
Daniel Dickison <danieldickison@gmail.com>
2009-09-27 12:28:25 -0500

    Added license info GPL for my code, other licenses for bsd-strlcat.c and the WidgetExample library

--- xplane-plugin/src/Preferences.c
+++ xplane-plugin/src/Preferences.c
@@ -1,10 +1,25 @@
 /*
  *  Preferences.c
- *  iX-Yoke-plugin
+ *  Wi-Fly-plugin
  *
  *  Created by Daniel Dickison on 5/11/09.
  *  Copyright 2009 Daniel_Dickison. All rights reserved.
  *
+ * 
+ *  This file is part of Wi-Fly-plugin.
+ *  
+ *  Wi-Fly-plugin is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  Wi-Fly-plugin is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with Wi-Fly-plugin.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "iX-Yoke-plugin.h"
